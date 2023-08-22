462f7a32698261d35d9320bb585260b5f3804b2e
Gabriel Terres <gabterres@hotmail.com>
2023-08-22 16:33:17 -0300

    Treinando comando de exclusão de banco

--- 1-CriandoBanco.sql
+++ 1-CriandoBanco.sql
@@ -1,1 +1,8 @@
+/* Criando Banco de dados principal onde será rodado comandos de testes */
 CREATE DATABASE [SUCOS_VENDAS];
+
+/* Criando Banco de dados apenas para treinamento do comando de exclusão */
+CREATE DATABASE [SUCOS_VENDAS_2];
+
+/* Excluindo Banco de dados */
+DROP DATABASE [SUCOS_VENDAS_2];
