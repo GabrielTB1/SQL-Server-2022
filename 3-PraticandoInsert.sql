bc6ef61927060a76c88e631d2ffd1be59fbec788
Gabriel Terres <gabterres@hotmail.com>
2023-08-24 08:28:05 -0300

    Praticando Insert Simples

---
+++ 3-PraticandoInsert.sql
@@ -0,0 +1,21 @@
+/* 
+DADOS DE TESTE: 
+
+Novo suco:
+1040107
+Light 350 ml - Melancia
+Lata
+350 ml
+Melancia
+R$ 4,56
+*/
+
+USE [SUCOS_VENDAS]
+INSERT INTO [TABELA DE PRODUTOS] VALUES (
+    '1040107',
+    'Light 350 ml - Melancia',
+    'Lata',
+    '350 ml',
+    'Melancia',
+    4.56
+);
