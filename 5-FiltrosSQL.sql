a78efab1641c4de2519cef4ad14508756936f4ec
Gabriel Terres <gabterres@hotmail.com>
2023-08-24 14:09:31 -0300

    Realizando consultas com filtro composto

--- 5-FiltrosSQL.sql
+++ 5-FiltrosSQL.sql
@@ -21,6 +21,20 @@ WHERE [PRECO DE LISTA] <= 7.00;
 SELECT * FROM [TABELA DE PRODUTOS] 
 WHERE [EMBALAGEM] > 'Lata';
 
--- Filtrando consulta com operador de maior em um campo de data -- 
+-- Filtrando consulta com operador de maior em um campo de data --
 SELECT [NOME],[DATA DE NASCIMENTO] FROM [TABELA DE CLIENTES]
 WHERE [DATA DE NASCIMENTO] > '1995-12-31';
+
+-- Filtrando consulta com operador de maior em um campo de data mas utilizando apenas o ano como filtro --
+SELECT [NOME], [DATA DE NASCIMENTO] FROM [TABELA DE CLIENTES]
+WHERE YEAR([DATA DE NASCIMENTO]) > '1995';
+
+-- Filtrando consulta  com operador de comparação igual e com operador lógico OR --
+SELECT [NOME], [BAIRRO]
+FROM [TABELA DE CLIENTES]
+WHERE [BAIRRO] = 'Copacabana' OR [BAIRRO] = 'Tijuca';
+
+-- Filtrando consulta  com operador de comparação igual e com operador lógico AND --
+SELECT [NOME], [ESTADO], [PRIMEIRA COMPRA] 
+FROM [TABELA DE CLIENTES]
+WHERE [PRIMEIRA COMPRA] = '1' AND [ESTADO] = 'SP';
