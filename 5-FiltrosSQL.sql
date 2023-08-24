578f17d2dd76c8afe9915f92a58605bc9a1efa8d
Gabriel Terres <gabterres@hotmail.com>
2023-08-24 11:49:22 -0300

    Utilizando operadores no filtro do SELECT

--- 5-FiltrosSQL.sql
+++ 5-FiltrosSQL.sql
@@ -5,3 +5,22 @@ SELECT nome_do_cliente, primeira_compra
 FROM clientes
 WHERE [PRIMEIRA COMPRA] = 1;
 
+-- Filtrando consulta com operador de igual --
+SELECT * FROM [TABELA DE PRODUTOS]
+WHERE [PRECO DE LISTA] = 7.00;
+
+-- Filtrando consulta com operador de maior que --
+SELECT * FROM [TABELA DE PRODUTOS]
+WHERE [PRECO DE LISTA] > 7.00;
+
+-- Filtrando consulta com operador de menor ou igual que --
+SELECT * FROM [TABELA DE PRODUTOS]
+WHERE [PRECO DE LISTA] <= 7.00;
+
+-- Filtrando consulta com operador de maior em uma string --
+SELECT * FROM [TABELA DE PRODUTOS] 
+WHERE [EMBALAGEM] > 'Lata';
+
+-- Filtrando consulta com operador de maior em um campo de data -- 
+SELECT [NOME],[DATA DE NASCIMENTO] FROM [TABELA DE CLIENTES]
+WHERE [DATA DE NASCIMENTO] > '1995-12-31';
